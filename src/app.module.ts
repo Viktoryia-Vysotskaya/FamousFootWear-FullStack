a374eb310b5dc94b97c2e4c34f423bc1d48ae20f
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2024-01-06 18:10:05 +0100

    Add auth, cart, orders, products, and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,14 @@ import {
 import * as cors from 'cors';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { CartModule } from './cart/cart.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, CartModule, OrdersModule, ProductsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
